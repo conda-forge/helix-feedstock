bda1319ecac38a512cc5203a2792077fecc2af78
Joshua A. Anderson <joaander@umich.edu>
2025-07-16 09:16:28 -0400

    Attempt to force use of endian.h

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,6 @@ cargo-bundle-licenses \
 HELIX_LIBEXEC="$PREFIX"/libexec/helix
 
 # build statically linked binary with Rust
-export CFLAGS="-DHAVE_ENDIAN_H"
 RUSTFLAGS="-C link-args=-Wl,-headerpad_max_install_names" cargo install --profile opt --locked --no-track --root "$HELIX_LIBEXEC" --path helix-term
 
 # move executable to $HELIX_LIBEXEC
